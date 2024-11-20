1500425fbba842c72f28d15bfa2c8a6131daf7d2
Yutaka Ando <yampan@hotmail.co.jp>
2024-11-20 22:59:56 +0900

    2024-11-20_final

--- JROD-gui.py
+++ JROD-gui.py
@@ -45,7 +45,8 @@ f_dic = {}
 for f in font_items2:
     if f[0:1] in ['@', "$", "%", "&"]: continue
     elif "HGrep" in f or "CR" in f or "AR" in f or "HG" in f or \
-        "Jsut" in f or "ＤＦ" in f or "明朝" in f or "TA" in f:
+        "Jsut" in f or "ＤＦ" in f or "明朝" in f or "TA" in f or \
+          "symbol" in f.lower():
         continue
     font_items.append(f)
     f_dic[f] = 1
@@ -70,10 +71,25 @@ layout = [
         )
     ],
     [eg.Input("-", key="-font-", expand_x=True), eg.Button("Copy")],
-    [eg.Button("Exit", color="#FF2222", font=("Arial",16,"bold"))],
+    [eg.Button("font +"), eg.Button("font -"), eg.Text("   "),
+     eg.Button("Exit", color="#FF2222", font=("Arial",16,"bold"))],
 ]
 # create Window
-with eg.Window("Font List", layout, font=("Arial", f_size), finalize=True, resizable=True) as window:
+flag = 1
+with eg.Window("Font List", layout, font=("Arial", f_size), finalize=True, 
+               resizable=True, center_window=False, location=(100,100)) as window:
+    if flag:
+        flag = 0
+        
+        print("get_center_location=", window.get_center_location())
+        print("get_screen_size=", window.get_screen_size())
+        aaa = 0.95
+        print("set_alpha_channel=", aaa)
+        window.set_alpha_channel(aaa)
+        w_size = (600,600)
+        print("set_size=", w_size)
+        window.set_size(w_size)
+        print("get_size=", window.get_size())
     # set_theme
     eg.set_theme("clam")
     ''' - macOS --- ('aqua', 'clam', 'alt', 'default', 'classic')
@@ -84,14 +100,19 @@ with eg.Window("Font List", layout, font=("Arial", f_size), finalize=True, resiz
         print("# event:", event, values)
         if event == "Exit" or event == eg.WINDOW_CLOSED:
             break
-        if event == "-fontlist-":
+        if event in ["-fontlist-", "font +", "font -"]:
+            if event == "font +": f_size += 1
+            if event == "font -": f_size -= 1
+            print("f_size=", f_size)
             # get font name from listbox
             fontlist: eg.Listbox = window["-fontlist-"]
             index = fontlist.get_cursor_index()
             if index >= 0:
                 font_name = font_items[index]
                 window["-font-"].update(font_name)
-                window["-sample-"].update(font=(font_name, 18))
+                #window["-sample-"].update(font=(font_name, 18))
+                window["-sample-"].set_text(f"Hello, 123 こんにちは?  Size:{f_size}")
+                window["-sample-"].update(font=(font_name, f_size))
         if event == "Copy":
             eg.set_clipboard(values["-font-"])
             eg.print("Copied to clipboard:\n" + values["-font-"])
